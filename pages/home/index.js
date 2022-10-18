6c6c8f6feed0fe88d33847a1fa807c64e0409725
Saniel1990 <saniel_sabr@outlook.com>
2022-10-18 20:08:22 -0300

    m2-gitSearch-Saniel1990 first commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,102 @@
 /* Desenvolva sua lógica aqui...*/
+const apiBase = "https://api.github.com/users"
+const recentUsersContainer = document.getElementById('cards_users_box')
+
+const recentUsersArray = getUsersFoundeds();
+console.log(recentUsersArray);
+
+
+function userFind(url) {
+    const buttonSearch = document.getElementById('user_search_bttn');
+    buttonSearch.addEventListener('click', (event) => {
+        event.preventDefault();
+        const name = document.getElementById('user_search').value;
+        if (name !== '') {
+            fetch(`${url}/${name}`, { method: 'GET', headers: { 'Content-Type': 'aplication/json' } })
+                .then((response) => response.json())
+                .then((response) => {
+                    if(response.message == 'Not Found'){
+                        const alertSpan = document.getElementById('alert_span');
+                        alertSpan.innerText = "Usuário não encontrado!"
+                        alertSpan.style.color = "var(--color-alert)"
+                    }
+                    else{
+                        addNRevomeUser(response);
+                        addSingleUser(response);
+                    }
+                }).catch((error) => console.log(error))
+            window.location.assign("../profile/index.html")
+        }
+    })
+}
+
+function renderAnything(array, tagContainer, creatorFunction) {
+    tagContainer.innerHTML = '';
+    array.forEach(user => {
+        const newCard = creatorFunction(user, array);
+        tagContainer.appendChild(newCard);
+    });
+}
+
+function recentUsersCreator(user) {
+    const tagLi = document.createElement('li');
+    const tagLink = document.createElement('a');
+    const tagImg = document.createElement('img');
+    const tagBttn = document.createElement('button');
+    
+    tagLi.classList.add("recent-users-pictures");
+    tagLink.classList.add("recent-users-link");
+    
+    tagImg.src = `${user.avatar_url}`;
+    tagImg.alt = user.name;
+    tagBttn.innerText = 'Acessar este perfil'
+    
+    tagLink.append(tagImg, tagBttn);
+    tagLi.appendChild(tagLink);
+    return tagLi    
+}
+
+/*
+<li class="recent-users-pictures">
+<a class="recent-users-link" href=""><img src="" alt=""><button class="button button-link">Acessar este perfil</button></a>
+</li>
+*/
+
+
+
+function addSingleUser(user) {
+    let myOnlyser = getSingleUser();
+    if (myOnlyser.length >= 1) {
+        myOnlyser.splice(0, 1);
+        myOnlyser = [user, ...myOnlyser];
+    } else {
+        myOnlyser = [user, ...myOnlyser];
+    }
+    localStorage.setItem("@mySingleUser:SingleUser", JSON.stringify(myOnlyser))
+}
+
+function getSingleUser() {
+    return JSON.parse(localStorage.getItem("@mySingleUser:SingleUser")) || [];
+}
+
+function addNRevomeUser(user) {
+    let selectedUsers = getUsersFoundeds();
+    if (selectedUsers.length >= 3) {
+        selectedUsers.splice(2, 2);
+        selectedUsers = [user, ...selectedUsers];
+    } else {
+        selectedUsers = [user, ...selectedUsers];
+    }
+    localStorage.setItem("@recentUsers:userFound", JSON.stringify(selectedUsers))
+}
+
+function getUsersFoundeds() {
+    return JSON.parse(localStorage.getItem("@recentUsers:userFound")) || [];
+}
+
+function checkExistence(user) {
+    return getUsersFoundeds().findIndex(element => element.login === user.login)
+}
+
+renderAnything(recentUsersArray, recentUsersContainer, recentUsersCreator)
+userFind(apiBase);
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,125 @@
 /* Desenvolva sua lógica aqui...*/
+const tagUl = document.getElementById('cards_container');
+const tagContainer = document.getElementById('user_info');
+let myCurrentUser = getSingleUser();
+
+function findRepos(url) {
+    fetch(`${url}`, { method: 'GET', headers: { 'Content-Type': 'aplication/json' } })
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.length !== 0) {
+                renderAnything(response, tagUl, cardCreate)                
+            }else{
+                tagUl.innerHTML = `
+                <div class="not-found-repos">
+                    <p class='text-1-2'>Este usuário ainda não tem repositórios.</p>
+                </div>`
+            }
+        }).catch((error) => console.log(error))
+}
+
+headerCreator(myCurrentUser)
+
+function getSingleUser() {
+    return JSON.parse(localStorage.getItem("@mySingleUser:SingleUser")) || [];
+}
+
+function headerCreator(currentUser) {
+    const myUser = currentUser[0];
+    tagContainer.innerHTML = '';
+    const tagImg = document.createElement('img');
+    const tagDiv = document.createElement('div');
+    const tagTitle = document.createElement('h1');
+    const tagSpan = document.createElement('span');
+
+    tagImg.classList.add("profile-picture");
+    tagDiv.classList.add('profile-descriptions');
+    tagTitle.classList = "title-1 profile-title";
+    tagSpan.classList.add("profile-stack");
+
+    tagImg.src = `${myUser.avatar_url}`;
+    tagImg.alt = myUser.login;
+    tagTitle.innerText = myUser.login;
+    if (myUser.name == null) {
+        tagTitle.innerText = myUser.login;
+    } else {
+        tagTitle.innerText = myUser.name;
+    }
+
+    if (myUser.bio == null) {
+        tagSpan.innerText = 'Usuário sem bio.';
+    } else {
+        tagSpan.innerText = myUser.bio;
+    }
+
+    tagDiv.append(tagTitle, tagSpan)
+    tagContainer.append(tagImg, tagDiv)
+    findRepos(myUser.repos_url)
+}
+
+function renderAnything(array, tagContainer, creatorFunction) {
+    tagContainer.innerHTML = '';
+    array.forEach(element => {
+        const newCard = creatorFunction(element);
+        tagContainer.appendChild(newCard);
+    });
+}
+
+function cardCreate(reposItens) {
+    console.log(reposItens)
+    const tagLi = document.createElement('l1');
+    tagLi.classList.add('card-item');
+
+    const tagTitle = document.createElement('h2');
+    tagTitle.classList = 'title-2-1 card-title';
+
+    const tagParagraph = document.createElement('p');
+    tagParagraph.classList = 'text-1-2 card-description';
+
+    const tagDiv = document.createElement('div');
+    tagDiv.classList.add('interactive-box');
+
+    const tagButton1 = document.createElement('button');
+    tagButton1.classList = "text-2-button button card-button"
+
+    const tagButton2 = document.createElement('button');
+    tagButton2.classList = "text-2-button button card-button"
+
+    tagButton1.innerText = 'Repositório';
+    tagButton2.innerText = 'Demo';
+
+    if (reposItens.name) {
+        tagTitle.innerText = reposItens.name;
+    } else {
+        tagTitle.innerText = reposItens.login;
+    }
+
+    if (reposItens.description !== null) {
+        tagParagraph.innerText = reposItens.description;        
+    }else{
+        tagParagraph.innerText = `Trabalho de ${reposItens.full_name}, em ${reposItens.language}`;
+    }
+
+    tagDiv.append(tagButton1, tagButton2);
+    tagLi.append(tagTitle, tagParagraph, tagDiv);
+    return tagLi;
+}
+
+function getUsersFoundeds() {
+    return JSON.parse(localStorage.getItem("@recentUsers:userFound")) || [];
+}
+
+function checkExistence(user) {
+    return getUsersFoundeds().findIndex(element => element.login === user.login)
+}
+
+function addNRevomeUser(user) {
+    let selectedUsers = getUsersFoundeds();
+    if (selectedUsers.length > 3) {
+        selectedUsers.splice(selectedUsers.length - 1, 1);
+        selectedUsers = [user, ...selectedUsers];
+    } else {
+        selectedUsers = [user, ...selectedUsers];
+    }
+    localStorage.setItem("@recentUsers:userFound", JSON.stringify(selectedUsers))
+}
